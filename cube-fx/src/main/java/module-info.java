9619bc56c24187a8b94be4f693839fcb353f9bff
Maxime MOSELLE <maximemoselle@gmail.com>
2019-08-30 21:36:21 +0200

    Ajustement des dépendances

--- cube-fx/src/main/java/module-info.java
+++ cube-fx/src/main/java/module-info.java
@@ -5,7 +5,6 @@ module cube.fx {
     
     requires javafx.base;
     requires javafx.graphics;
-    requires javafx.controls;
 
     exports com.github.zelmothedragon.cube.fx;
     exports com.github.zelmothedragon.cube.fx.engine;
