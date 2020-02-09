6a7402fec8c4d6c7ee81455abf48435f931aa4c2
Maxime MOSELLE <maximemoselle@gmail.com>
2020-02-09 19:28:27 +0100

    Ajout Javadoc et centrer la caméra sur les rectangles

--- cube-core/src/main/java/com/github/zelmothedragon/cube/core/entity/geometry/Camera.java
+++ cube-core/src/main/java/com/github/zelmothedragon/cube/core/entity/geometry/Camera.java
@@ -3,34 +3,70 @@ package com.github.zelmothedragon.cube.core.entity.geometry;
 import com.github.zelmothedragon.cube.core.entity.Component;
 
 /**
+ * Caméra.
  *
  * @author MOSELLE Maxime
  */
 public final class Camera implements Component {
 
+    /**
+     * Instance unique.
+     */
     public static final Camera INSTANCE = new Camera();
 
+    /**
+     * Position en abcisse.
+     */
     private int xp;
 
+    /**
+     * Position en ordonnée.
+     */
     private int yp;
 
+    /**
+     * Constructeur interne, pas d'instanciation.
+     */
     private Camera() {
+        this.xp = 0;
+        this.yp = 0;
     }
 
+    /**
+     * Suivre un point avec cette caméra.
+     *
+     * @param point Un point à suivre
+     */
     public void follow(final Point point) {
         this.xp = point.getXp();
         this.yp = point.getYp();
     }
 
+    /**
+     * Suivre un rectangle avec cette caméra.
+     *
+     * @param rectangle Un rectangle
+     */
     public void follow(final Rectangle rectangle) {
-        this.xp = rectangle.getXp();
-        this.yp = rectangle.getYp();
+        // Centrer la caméra
+        this.xp = rectangle.getXp() + rectangle.getWidth() / 2;
+        this.yp = rectangle.getYp() + rectangle.getHeight() / 2;
     }
 
+    /**
+     * Accesseur, obtenir la position en abcisse.
+     *
+     * @return La position en abcisse
+     */
     public int getXp() {
         return xp;
     }
 
+    /**
+     * Accesseur, obtenir la position en ordonnée.
+     *
+     * @return La position en ordonnée
+     */
     public int getYp() {
         return yp;
     }
--- cube-core/src/main/java/com/github/zelmothedragon/cube/core/system/CameraSystem.java
+++ cube-core/src/main/java/com/github/zelmothedragon/cube/core/system/CameraSystem.java
@@ -13,36 +13,42 @@ import com.github.zelmothedragon.cube.core.graphic.Render;
  * @author MOSELLE Maxime
  */
 public final class CameraSystem extends AbstractSystem {
-    
+
     public CameraSystem(final GameContainer gc, final int priority) {
         super(gc, priority);
     }
-    
+
     @Override
     public void update() {
-        
+
         gc
                 .getEntities()
                 .filter(Camera.class)
                 .stream()
                 .forEach(CameraSystem::updateCamera);
-        
+
     }
-    
+
     @Override
     public void draw(final Render g2d) {
-        
+
+        // Appliquer la caméra pour le rendu.
         gc
                 .getEntities()
                 .filter(Camera.class)
                 .stream()
                 .map(e -> e.getComponent(Camera.class))
-                .forEach(c -> g2d.setOffset(c));
-        
+                .forEach(g2d::setOffset);
+
     }
-    
+
+    /**
+     * Faire suivre la caméra par rapport à une entité.
+     *
+     * @param entity Une entité possédant l'instance de la caméra
+     */
     private static void updateCamera(final Entity entity) {
-        
+
         var camera = entity.getComponent(Camera.class);
         if (entity.hasComponent(Point.class)) {
             var point = entity.getComponent(Point.class);
@@ -54,5 +60,5 @@ public final class CameraSystem extends AbstractSystem {
             // RAS
         }
     }
-    
+
 }
